ea30358eb8ed8802a3312df74af32ed563552faa
Squawylaous <57704977+Squawylaous@users.noreply.github.com>
2022-03-22 13:54:52 -0500

    Update misc.py

--- misc.py
+++ misc.py
@@ -8,26 +8,27 @@ foreground = Color(255, 255, 255)
 update_rects = []
 
 # variables to store constant values
-UI_offset = vector(50, 250) # how much space the UI needs
+UI_offset = vector(25, 50) # how much space the UI needs
 surface_scale = vector() # size of mazes
+surface_padding = vector(10, 10) # empty space around a surface
+surface_scale_padded = surface_scale+surface_padding
 NEXTLEVEL = USEREVENT + 0
 all_levels = ["l1"] # all levels, in order
-
-current_level = -1
 
 def get_sign(x):
   return 1 if x>0 else -1 if x<0 else 0
 
 # transforms a vector to position on a surface
 def to_surface_pos(pos, size):
-  return tuple(map(int, surface_scale.elementwise()*vector(*pos)/size+(5,5)))
+  return tuple(map(int, surface_scale.elementwise()*vector(*pos)/size+surface_padding/2))
 
 
 # singleton, esentally a dict w/ some dynamic values
 class level_names_class:
   # keys are level codes (used internally) and values are displayed names
-  def __init__(self):
+  def __init__(self, start=-1):
     self.data = {}
+    self.current_level = start
   
   def __getitem__(self, key):
     try:
@@ -39,6 +40,5 @@ class level_names_class:
     return self.data[key]
   
   def __next__(self):
-    global current_level
-    current_level += 1
-    return all_levels[current_level]
+    self.current_level += 1
+    return all_levels[self.current_level]
