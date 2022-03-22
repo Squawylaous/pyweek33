93925ca45b1b10e0ba0baf3c0e9bb5a420bb9697
Squawylaous <57704977+Squawylaous@users.noreply.github.com>
2022-03-22 11:23:20 -0500

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,8 +1,23 @@
+from pygame.locals import *
 from pygame.math import Vector2 as vector
 
 # just some miscellaneous functions grouped togther so they can be imported anywhere
 
+background = Color(0, 0, 0)
+foreground = Color(255, 255, 255)
+update_rects = []
+
+# variables to store constant values
 surface_scale = 300
+NEXTLEVEL = USEREVENT + 0
+all_levels = ["l1"] # all levels, in order
+
+# variables for storing references to data, can be updated
+current_level = -1
+player = None
+twin = None
+current_level_p = None
+current_level_t = None
 
 def get_sign(x):
   return 1 if x>0 else -1 if x<0 else 0
@@ -10,3 +25,24 @@ def get_sign(x):
 # transforms a vector to position on a surface
 def to_surface_pos(pos, size):
   return tuple(map(int, vector(*pos)/size*surface_scale))
+
+
+# singleton, esentally a dict w/ some dynamic values
+class level_names_class:
+  # keys are level codes (used internally) and values are displayed names
+  def __init__(self):
+    self.data = {}
+  
+  def __getitem__(self, key):
+    try:
+      return self.data[key]
+    except KeyError:
+      pass
+    if key[0] == "l":
+      self.data[key] = "Level "+key[1:]
+    return self.data[key]
+  
+  def __next__(self):
+    global current_level
+    current_level += 1
+    return all_levels[current_level]
