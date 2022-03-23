03366246b0643d1a4c5fc56ef7d3f89ded510f53
Squawylaous <57704977+Squawylaous@users.noreply.github.com>
2022-03-23 13:54:03 -0500

    Update misc.py

--- misc.py
+++ misc.py
@@ -9,18 +9,21 @@ update_rects = []
 
 # variables to store constant values
 UI_offset = vector(25, 50) # how much space the UI needs
-surface_scale = vector() # size of mazes
-surface_padding = vector(10, 10) # empty space around a surface
-surface_scale_padded = surface_scale+surface_padding
+surface_scale_real = vector() # size of mazes scaled to fit screen
+surface_scale = vector(1000, 1000) # size of mazes
+surface_padding = vector(100, 100) # empty space around a surface
 NEXTLEVEL = USEREVENT + 0
+direction_keys=[K_UP, K_DOWN, K_LEFT, K_RIGHT]
 all_levels = ["l1"] # all levels, in order
 
 def get_sign(x):
   return 1 if x>0 else -1 if x<0 else 0
 
+group_dict = lambda dict_: {i:dict_[key] for key in dict_ for i in key}
+
 # transforms a vector to position on a surface
 def to_surface_pos(pos, size):
-  return tuple(map(int, surface_scale.elementwise()*vector(*pos)/size+surface_padding/2))
+  return tuple(map(int, surface_scale.elementwise()*vector(*pos)+surface_padding/2))
 
 
 # singleton, esentally a dict w/ some dynamic values
@@ -42,6 +45,7 @@ class level_names_class:
   def __next__(self):
     self.current_level += 1
     return all_levels[self.current_level]
+
 
 # basically a mix of javasctipt objects and default dicts, just for convience
 class container:
