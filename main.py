917dbd8b907d0a6b41591c19542fae36bc2b0bfb
Squawylaous <57704977+Squawylaous@users.noreply.github.com>
2022-03-23 13:48:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,14 @@ pygame.init()
 screen = pygame.display.set_mode((0, 0), FULLSCREEN)
 screen_rect = screen.get_rect()
 
-# surfaces to draw mazes on
+# surface to draw mazes on
 main_surface_rect = pygame.rect.Rect(UI_offset, vector(screen_rect.size)-UI_offset)
 main_surface = screen.subsurface(main_surface_rect)
 
 # variables to store constant values
-surface_scale.x = min(main_surface_rect.w/2, main_surface_rect.h)
-surface_scale.y = surface_scale.x
-surface_scale_padded += surface_scale
-twin_pos = -(surface_scale_padded-(main_surface_rect.w/2, main_surface_rect.h))/2
+surface_scale_real.x = min(main_surface_rect.w/2, main_surface_rect.h)
+surface_scale_real.y = surface_scale_real.x
+twin_pos = -(surface_scale_real-(main_surface_rect.w/2, main_surface_rect.h))/2
 player_pos = twin_pos + (main_surface_rect.w/2, 0)
 
 # variables for storing references to data, can be updated
@@ -37,8 +36,9 @@ class entity:
   def __init__(self, maze, isPlayer, draw):
     self.maze, self.isPlayer, self._draw = maze, isPlayer, draw
     self.pos = self.maze.start
+    self.maze.player = self
     entity.all.append(self)
-    self.surface = pygame.Surface(surface_scale/self.maze.size)
+    self.surface = pygame.Surface(surface_scale*0.75)
     self.rect = self.surface.get_rect()
 
   def move(self, direction):
@@ -57,7 +57,7 @@ class entity:
   
   def draw(self):
     self._draw(self)
-    self.maze.blit(self.surface, to_surface_pos(self.pos-(0.5, 0.5), self.maze.size))
+    self.maze.surface.blit(self.surface, to_surface_pos(self.pos-(0.375, 0.375), self.maze.size))
   
   @staticmethod
   def act(action, *, check_ifs=None):
@@ -85,7 +85,7 @@ class entity:
 # class for mazes, just a collection for walls
 class maze:
   all = {};
-  def __init__(self, name, size=None, start=(0,0), finish=(0,0), *, walls):
+  def __init__(self, name, pos, size=None, start=(0,0), finish=(0,0), *, walls):
     self.name = name.split("_")
     walls = chain(*[zip(wall, wall[1:]) for wall in walls])
     self.perm_walls = [*map(elements.Wall, chain(*map(split_wall, walls)))]
@@ -95,15 +95,16 @@ class maze:
       size = max(all_pos) - min(all_pos)
     self.size = size
     self.start, self.finish = vector(start), elements.Finish(finish)
-    
     if self.name[0] not in maze.all:
       maze.all[self.name[0]] = {}
     maze.all[self.name[0]][self.name[1]] = self
-    self.background = pygame.Surface(surface_scale_padded)
+    
+    self.background = pygame.Surface(surface_scale*self.size+surface_padding)
     for element in self.still_elements:
       element.draw(self.background, self.size)
-    self.surface = pygame.Surface(surface_scale_padded)
-    self.rect = self.surface.get_rect()
+    self.surface = pygame.Surface(surface_scale*self.size+surface_padding)
+    self.rect = pygame.rect.Rect(pos, surface_scale_real)
+    self.scaled_surface = main_surface.subsurface(self.rect)
 
   def __getitem__(self, item):
     for element in self.elements:
@@ -126,14 +127,21 @@ class maze:
     self.surface.blit(self.background, (0,0))
     for element in self.anim_elements:
       element.draw(self.surface, self.size)
+    self.player.draw()
+    pygame.transform.scale(self.surface, self.rect.size, self.scaled_surface)
 
 
 # functions for drawing entities
 def draw_entity(color):
-  color = Color(color)
+  color = Color(*color)
   
   def draw_square(self):
-    draw_rect = pygame.rect.Rect((0,0), vector(self.rect.size)*0.75)
+    draw_rect = pygame.rect.Rect((0,0), vector(self.rect.size))
+    draw_rect.center = self.rect.center
+    pygame.draw.rect(self.surface, color, draw_rect)
+  
+  def draw_square(self):
+    draw_rect = pygame.rect.Rect((0,0), vector(self.rect.size))
     draw_rect.center = self.rect.center
     pygame.draw.rect(self.surface, color, draw_rect)
   
@@ -160,24 +168,20 @@ def load_level(level):
   screen.fill(background)
   current_level_p = maze.all[level]["p"]
   current_level_t = maze.all[level]["t"]
-  player = entity(current_level_p, True, draw_entity((161,161,161)).square)
-  twin = entity(current_level_t, False)
+  player = entity(current_level_p, True, draw_entity((161, 161, 161)).square)
+  twin = entity(current_level_t, False, draw_entity((161, 145, 145)).square)
   current_level_p.draw()
   current_level_t.draw()
-  player.draw()
-  twin.draw()
-  main_surface.blit(current_level_p.surface, player_pos)
-  main_surface.blit(current_level_t.surface, twin_pos)
   update_rects.append(main_surface_rect)
 
 level_names = level_names_class()
 
-maze("l1_p", size=7, start=(4.5, 6.5), finish=(2.5,0.5),
+maze("l1_p", player_pos, size=7, start=(4.5, 6.5), finish=(2.5,0.5),
      walls=[((1, 1), (1, 6), (4, 6), (4, 7), (5, 7), (5, 6), (6, 6), (6, 1), (3, 1), (3, 0), (2, 0), (2, 1), (1, 1)),
             ((3, 1), (3, 2), (4, 2)), ((2, 2), (2, 3), (3, 3), (3, 5), (4, 5)), ((1, 4), (2, 4)),
             ((2, 5), (2, 6)), ((5, 5), (6, 5)), ((4, 4), (6, 4)), ((4, 3), (6, 3)), ((5, 2), (5, 3))]
 )
-maze("l1_t", size=7, start=(4.5, 6.5), finish=(2.5,0.5),
+maze("l1_t", twin_pos, size=7, start=(4.5, 6.5), finish=(2.5,0.5),
      walls=[((1, 1), (1, 6), (4, 6), (4, 7), (5, 7), (5, 6), (6, 6), (6, 1), (3, 1), (3, 0), (2, 0), (2, 1), (1, 1)),
             ((2, 1), (2, 3)), ((1, 4), (3, 4), (3, 3)), ((3, 2), (4, 2), (4, 4)), ((2, 6), (2, 5), (4, 5)),
             ((5, 2), (5, 3), (6, 3)), ((5, 4), (5, 6))]
@@ -190,7 +194,9 @@ while True:
     break
   for event in pygame.event.get():
     if event.type == KEYDOWN:
-      if event.key == K_RETURN:
+      if event.key in direction_keys:
+        print(event)
+      elif event.key == K_RETURN:
         pygame.event.post(pygame.event.Event(NEXTLEVEL))
       elif event.key == K_ESCAPE:
         pygame.event.post(pygame.event.Event(QUIT))
