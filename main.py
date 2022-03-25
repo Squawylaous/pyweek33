f3eb154e51788d08d867b9a2cb98e8a5dd12e5b9
Squawylaous <57704977+Squawylaous@users.noreply.github.com>
2022-03-25 16:59:08 -0500

    almost done

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ screen_rect = screen.get_rect()
 font = pygame.font.SysFont("", 75)
 screen.blit(font.render("Loading...", 1, foreground), (0, screen_rect.h-font.size("Loading...")[1]))
 pygame.display.flip()
-current_state = container(screen="menu", input=False)
+current_state = container(screen="menu")
 
 
 # surface to draw mazes on
@@ -40,7 +40,7 @@ class entity:
   
   def __init__(self, maze, isPlayer, draw):
     self.maze, self.isPlayer, self._draw = maze, isPlayer, draw
-    self.pos = self.maze.start
+    self.pos = vector(self.maze.start)
     self.maze.player = self
     entity.all.append(self)
     self.surface = pygame.Surface(surface_scale*0.75)
@@ -76,11 +76,10 @@ class entity:
       if i == "stop":
         if action[i] == "before":
           do.move = -direction
-        elif action[i] == "return":
-          do.move = -direction*dist
       elif i == "is":
         if action[i]["?"] == "direction":
-          entity.act(element, action[i]["T" if direction == element.direction else "F"], direction=direction, do=do)
+          entity.act(element, action[i]["T" if vector(*map(get_sign, direction)) == element.direction else "F"],
+                     direction=direction, do=do)
       elif i == "if":
         do.check_ifs.append((element, action[i]))
     
@@ -93,10 +92,10 @@ class entity:
 
 
 # class for mazes, just a collection for walls
-class maze:
+class Maze_class:
   all = {};
   def __init__(self, name, pos, size=None, start=(0,0), finish=(0,0), *, walls, one_way_walls=[]):
-    self.name = name.split("_")
+    self.name = name
     self.start, self.finish = vector(start), elements.Finish(finish)
     walls = chain(*[zip(wall, wall[1:]) for wall in walls])
     self.perm_walls = [*map(elements.Wall, chain(*map(split_wall, walls)))]
@@ -107,10 +106,6 @@ class maze:
       all_pos = {*chain(*map(op.attrgetter("pos"), self.elements))}
       size = max(all_pos) - min(all_pos)
     self.size = size
-    if self.name[0] not in maze.all:
-      maze.all[self.name[0]] = {}
-    maze.all[self.name[0]][self.name[1]] = self
-    
     self.background = pygame.Surface(surface_scale*self.size+surface_padding)
     for element in self.still_elements:
       element.draw(self.background, self.size)
@@ -227,19 +222,20 @@ menu_buttons = button(screen, menu_button_func, menu_button_init, (1, 4), (300, 
                       exit=container(text="Exit Game", pos=(0, 3), event=QUIT)
 )
 
-select_buttons = button(screen, select_button_func, select_button_init, (5, 2), vector(screen_rect.size)*0.875/max(len(all_levels)//5, 5),
-                        screen_rect.center, **{all_levels[i]:container(pos=(i%5, i//5)) for i in range(len(all_levels))})
+row_size = 3
+select_buttons = button(screen, select_button_func, select_button_init, (row_size, len(all_levels)/row_size),
+                        vector(screen_rect.size)*0.875/max(len(all_levels)//row_size, row_size),
+                        screen_rect.center, **{all_levels[i]:container(pos=(i%row_size, i//row_size)) for i in range(len(all_levels))})
 
 # returns decorator function
 def load_screen(current_screen):
-  # decorator function, updates current_state and turns off user input untill function finishes running
+  # decorator function, updates current_state
   def load_decorator(func):
     def load_func(*args, **kwargs):
-      current_state.update(override=True, screen=current_screen, input=False)
+      current_state.screen=current_screen
       screen.fill(background)
       func(*args, **kwargs)
       update_rects.append(screen_rect)
-      current_state.input = True
     return load_func
   return load_decorator
 
@@ -257,8 +253,8 @@ def load_select():
 @load_screen("level")
 def load_level(level):
   global player, twin, current_level_p, current_level_t
-  current_level_p = maze.all[level]["p"]
-  current_level_t = maze.all[level]["t"]
+  current_level_p = Maze_class.all[level]["p"]()
+  current_level_t = Maze_class.all[level]["t"]()
   player = entity(current_level_p, True, draw_entity((161, 161, 161)).square)
   twin = entity(current_level_t, False, draw_entity((161, 130, 130)).square)
   level_text = level_names.current
@@ -271,25 +267,31 @@ def update_level():
   current_level_t.draw()
   update_rects.append(main_surface_rect)
 
+# creates a function that creates and returns a new maze object when called
+def maze(name, *args, **kwargs):
+  name = name.split("_")
+  if name[0] not in Maze_class.all:
+    Maze_class.all[name[0]] = {}
+  Maze_class.all[name[0]][name[1]] = lambda:Maze_class(name, *args, **kwargs)
 
 # all of the mazes for each level (didn't have time to put this in a .txt file or something so it's here)
 #level 1
-maze("l1_p", player_pos, size=5, start=(2.5, 4.5), finish=(4.5, 0.5),
-     walls=[((0, 0), (0, 5), (5, 5), (5, 0), (0, 0)),
-            ((0, 2), (1, 2)), ((2, 2), (3, 2), (3, 3), (2, 3), (2, 2)), ((4, 2), (5, 2)), ((1, 1), (4, 1))]
+maze("l1_p", player_pos, size=7, start=(3.5, 5.5), finish=(5.5, 1.5),
+     walls=[((1, 1), (1, 6), (6, 6), (6, 1), (1, 1)),
+            ((1, 3), (2, 3)), ((3, 3), (4, 3), (4, 4), (3, 4), (3, 3)), ((5, 3), (6, 3)), ((2, 2), (5, 2))]
 )
-maze("l1_t", twin_pos, size=5, start=(2.5, 4.5), finish=(4.5, 0.5),
-     walls=[((0, 0), (0, 5), (5, 5), (5, 0), (0, 0)),
-            ((0, 2), (1, 2)), ((2, 2), (3, 2), (3, 3), (2, 3), (2, 2)), ((4, 2), (5, 2)), ((1, 1), (5, 1)), ((4, 0), (4, 1))]
+maze("l1_t", twin_pos, size=7, start=(3.5, 5.5), finish=(5.5, 1.5),
+     walls=[((1, 1), (1, 6), (6, 6), (6, 1), (1, 1)),
+            ((1, 3), (2, 3)), ((3, 3), (4, 3), (4, 4), (3, 4), (3, 3)), ((5, 3), (6, 3)), ((2, 2), (6, 2)), ((5, 1), (5, 2))]
 )
 #level 2
-maze("l2_p", player_pos, size=5, start=(2.5, 4.5), finish=(4.5, 0.5),
-     walls=[((0, 0), (0, 5), (5, 5), (5, 0), (0, 0)),
-            ((0, 2), (1, 2)), ((2, 2), (3, 2), (3, 3), (2, 3), (2, 2)), ((4, 2), (5, 2)), ((1, 1), (4, 1))]
+maze("l2_p", twin_pos, size=7, start=(3.5, 5.5), finish=(5.5, 1.5),
+     walls=[((1, 1), (1, 6), (6, 6), (6, 1), (1, 1)),
+            ((1, 3), (2, 3)), ((3, 3), (4, 3), (4, 4), (3, 4), (3, 3)), ((5, 3), (6, 3)), ((2, 2), (5, 2))]
 )
-maze("l2_t", twin_pos, size=5, start=(2.5, 4.5), finish=(4.5, 0.5),
-     walls=[((0, 0), (0, 5), (5, 5), (5, 0), (0, 0)),
-            ((0, 2), (1, 2)), ((2, 4), (2, 2), (3, 2), (3, 4)), ((4, 2), (5, 2)), ((0, 4), (1, 4), (1, 5)), ((4, 5), (4, 4), (5, 4))]
+maze("l2_t", twin_pos, size=7, start=(3.5, 5.5), finish=(5.5, 1.5),
+     walls=[((1, 1), (1, 6), (6, 6), (6, 1), (1, 1)),
+            ((1, 3), (2, 3)), ((3, 5), (3, 3), (4, 3), (4, 5)), ((5, 3), (6, 3)), ((1, 5), (2, 5), (2, 6)), ((5, 6), (5, 5), (6, 5))]
 )
 #level 3
 maze("l3_p", player_pos, size=7, start=(4.5, 6.5), finish=(2.5, 0.5),
@@ -303,7 +305,39 @@ maze("l3_t", twin_pos, size=7, start=(4.5, 6.5), finish=(2.5, 0.5),
             ((2, 1), (2, 3)), ((1, 4), (3, 4), (3, 3)), ((3, 2), (4, 2), (4, 4)), ((2, 6), (2, 5), (4, 5)),
             ((5, 2), (5, 3), (6, 3)), ((5, 4), (5, 6))]
 )
-
+#level 4
+maze("l4_p", player_pos, size=7, start=(1.5, 0.5), finish=(5.5, 6.5),
+     walls=[((2, 1), (6, 1), (6, 7), (5, 7), (5, 6), (1, 6), (1, 0), (2, 0), (2, 2)),
+            ((1, 3), (2, 3)), ((2, 5), (2, 6)), ((3, 5), (3, 4), (4, 4)), ((5, 4), (5 ,6)),
+            ((4, 2), (4, 3), (5, 3), (5, 1))]
+)
+maze("l4_t", twin_pos, size=7, start=(1.5, 0.5), finish=(5.5, 6.5),
+     walls=[((2, 1), (6, 1), (6, 7), (5, 7), (5, 6), (1, 6), (1, 0), (2, 0), (2, 3)),
+            ((3, 1), (3, 2)), ((1, 5), (2, 5)), ((3, 4), (3, 3), (4, 3)), ((4 ,2), (5 ,2), (5, 4)),
+            ((3, 6), (3, 5), (5, 5))]
+)
+#level 5
+maze("l5_p", player_pos, size=7, start=(3.5, 3.5), finish=(5.5, 6.5),
+     walls=[((1, 1), (6, 1), (6, 7), (5, 7), (5, 6), (1, 6), (1, 1)),
+            ((4, 3), (4, 5)), ((5, 4), (3, 4), (3, 3), (2, 3)), ((2, 2), (4, 2)), ((5, 1), (5, 3)),
+            ((3, 5), (3, 6)), ((2, 4), (2, 5), (1, 5))]
+)
+maze("l5_t", twin_pos, size=7, start=(3.5, 3.5), finish=(5.5, 6.5),
+     walls=[((1, 1), (6, 1), (6, 7), (5, 7), (5, 6), (1, 6), (1, 1)),
+            ((4, 3), (4, 4), (2, 4)), ((3, 3), (3, 4)), ((5, 2), (5, 6)), ((3, 5), (4, 5), (4, 6)),
+            ((1, 2), (2, 2), (2, 3)), ((3, 2), (5, 2), (5, 3)), ((5, 5), (5, 4), (6, 4))]
+)
+#level 6
+maze("l6_p", player_pos, size=10, start=(4.5, 6.5), finish=(2.5, 0.5),
+     walls=[((1, 1), (1, 6), (4, 6), (4, 7), (5, 7), (5, 6), (6, 6), (6, 1), (3, 1), (3, 0), (2, 0), (2, 1), (1, 1)),
+            ((3, 2), (4, 2)), ((2, 2), (2, 3), (3, 3), (3, 5), (4, 5)), ((1, 4), (2, 4)),
+            ((2, 5), (2, 6)), ((5, 5), (6, 5)), ((4, 4), (6, 4)), ((4, 3), (6, 3)), ((5, 2), (5, 3))]
+)
+maze("l6_t", twin_pos, size=10, start=(4.5, 6.5), finish=(2.5, 0.5),
+     walls=[((1, 1), (1, 6), (4, 6), (4, 7), (5, 7), (5, 6), (6, 6), (6, 1), (3, 1), (3, 0), (2, 0), (2, 1), (1, 1)),
+            ((2, 1), (2, 3)), ((1, 4), (3, 4), (3, 3)), ((3, 2), (4, 2), (4, 4)), ((2, 6), (2, 5), (4, 5)),
+            ((5, 2), (5, 3), (6, 3)), ((5, 4), (5, 6))]
+)
 
 post_event(MAINMENU)
 
@@ -314,33 +348,35 @@ while True:
     if event.type == KEYDOWN:
       if event.key == K_ESCAPE:
         post_event(QUIT if current_state.screen == "menu" else MAINMENU)
-      elif current_state.input:
-        if event.key in direction_keys:
-          direction = direction_keys[event.key]
-          if current_state.screen == "menu":
-            menu_buttons.move(direction)
-            menu_buttons.draw()
-          elif current_state.screen == "select":
-            select_buttons.move(direction)
-            select_buttons.draw()
-          elif current_state.screen == "level":
-            twin.move(direction)
-            player.move(direction)
-            update_level()
-        elif event.key == K_r:
-          goto_level()
-          post_event(NEXTLEVEL)
-        elif event.key in (K_RETURN, K_SPACE):
-          if current_state.screen == "menu":
-            menu_buttons.select()
-          elif current_state.screen == "select":
-            select_buttons.select()
+      elif event.key in direction_keys:
+        direction = direction_keys[event.key]
+        if current_state.screen == "menu":
+          menu_buttons.move(direction)
+          menu_buttons.draw()
+        elif current_state.screen == "select":
+          select_buttons.move(direction)
+          select_buttons.draw()
+        elif current_state.screen == "level":
+          twin.move(direction)
+          player.move(direction)
+          update_level()
+      elif event.key == K_r:
+        goto_level()
+        post_event(NEXTLEVEL)
+      elif event.key in (K_RETURN, K_SPACE):
+        if current_state.screen == "menu":
+          menu_buttons.select()
+        elif current_state.screen == "select":
+          select_buttons.select()
     elif event.type == MAINMENU:
       load_menu()
     elif event.type == LEVELSELECT:
       load_select()
     elif event.type == NEXTLEVEL:
-      load_level(next(level_names))
+      try:
+        load_level(next(level_names))
+      except KeyError:
+        post_event(MAINMENU)
     elif event.type in (LOSE, WIN):
       text = {LOSE:'Lose', WIN:'Win'}[event.type]
       text = f"You {text}!"
@@ -349,6 +385,7 @@ while True:
       pygame.time.wait(1500)
       if event.type == LOSE:
           goto_level()
+          pygame.event.clear(LOSE)
       post_event(NEXTLEVEL)
   
   pygame.display.update(update_rects)
