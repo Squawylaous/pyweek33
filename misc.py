74fd041324d1180f8d2cd2c67bab566c728203d0
Squawylaous <57704977+Squawylaous@users.noreply.github.com>
2022-03-25 10:06:11 -0500

    Update misc.py

--- misc.py
+++ misc.py
@@ -8,6 +8,9 @@ background = Color(0, 0, 0)
 foreground = Color(255, 255, 255)
 update_rects = []
 
+get_sign = lambda x: 1 if x>0 else -1 if x<0 else 0
+int_vector = lambda x:(int(round(x.x)), int(round(x.y)))
+ratio_vector = lambda x:vector(x)/max(x)
 group_dict = lambda dict_: {i:dict_[key] for key in dict_ for i in key}
 post_event = lambda event: pygame.event.post(pygame.event.Event(event))
 
@@ -19,21 +22,17 @@ surface_padding = vector(100, 100) # empty space around a surface
 MAINMENU = USEREVENT + 0
 LEVELSELECT = USEREVENT + 1
 NEXTLEVEL = USEREVENT + 2
-direction_keys = group_dict({(K_UP, K_w):"up", (K_DOWN, K_s):"down", (K_LEFT, K_a):"left", (K_RIGHT, K_d):"right"})
+direction_keys = group_dict({(K_UP, K_w):(0, -1), (K_DOWN, K_s):(0, 1), (K_LEFT, K_a):(-1, 0), (K_RIGHT, K_d):(1, 0)})
 all_levels = ["l1"] # all levels, in order
 
-def get_sign(x):
-  return 1 if x>0 else -1 if x<0 else 0
-
 # transforms a vector to position on a surface
-def to_surface_pos(pos, size):
-  return tuple(map(int, surface_scale.elementwise()*vector(*pos)+surface_padding/2))
+to_surface_pos = lambda pos, size: int_vector(surface_scale.elementwise()*vector(*pos)+surface_padding/2)
 
 
 # singleton, esentally a dict w/ some dynamic values
 class level_names_class:
   # keys are level codes (used internally) and values are displayed names
-  def __init__(self, start=-1):
+  def __init__(self, start=0):
     self.data = {}
     self.current_level = start
   
@@ -85,11 +84,31 @@ class container:
 
 # class for UI buttons the user can select. instances are groups
 class button:
-  def __init__(self, func, init_func, **options):
+  def __init__(self, screen, func, init_func, amount, size, center, **options):
     self.func = func
-    self.rect = screen_rect
+    self.size = vector(size)
+    self.amount = vector(amount)
+    self.rect = pygame.rect.Rect((0,0), self.size.elementwise()*(self.amount))
+    self.rect.center = center
     self.surface = screen.subsurface(self.rect)
-    self.options = {key:init_func(self, container(name=key, **value.dict)) for key, value in options.items()}
+    self.options = {value.pos:init_func(self, container(name=key, **value.dict)) for key, value in options.items()}
+    self.highlighted = None
   
-  def select(self, choice):
-    self._func(choic)
+  def select(self, choice=None):
+    if choice is None:
+      choice = self.highlighted
+    self.func(self, self.options[tuple(choice)])
+  
+  def move(self, direction):
+    if self.highlighted is None:
+      self.highlighted = vector()
+    else:
+      self.highlighted += direction
+      self.highlighted = vector([(h+a)%a for h, a in zip(self.highlighted, self.amount)])
+  
+  def draw(self):
+    for option in self.options.values():
+      option.surface.blit(option.background, (0,0))
+    if self.highlighted is not None:
+      pygame.draw.rect(self.surface, foreground, self.options[tuple(self.highlighted)].rect, 5)
+    update_rects.append(self.rect)
