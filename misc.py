3bcd4fadfe31a204e054675f7a5e70ecbf77e079
Squawylaous <57704977+Squawylaous@users.noreply.github.com>
2022-03-31 13:54:31 -0500

    Update misc.py

--- misc.py
+++ misc.py
@@ -12,7 +12,8 @@ get_sign = lambda x: 1 if x>0 else -1 if x<0 else 0
 int_vector = lambda x:(int(round(x.x)), int(round(x.y)))
 ratio_vector = lambda x:vector(x)/max(x)
 group_dict = lambda dict_: {i:dict_[key] for key in dict_ for i in key}
-post_event = lambda event: pygame.event.post(pygame.event.Event(event))
+post_event = lambda event, **kwargs: pygame.event.post(pygame.event.Event(event, kwargs))
+
 
 # variables to store constant values
 UI_offset = vector(0, 50) # how much space the UI needs
@@ -21,39 +22,13 @@ surface_scale = vector(1000, 1000) # size of mazes
 surface_padding = vector(100, 100) # empty space around a surface
 MAINMENU = USEREVENT + 0
 LEVELSELECT = USEREVENT + 1
-NEXTLEVEL = USEREVENT + 2
+NEWLEVEL = USEREVENT + 2
 LOSE = USEREVENT + 3
 WIN = USEREVENT + 4
 direction_keys = group_dict({(K_UP, K_w):(0, -1), (K_DOWN, K_s):(0, 1), (K_LEFT, K_a):(-1, 0), (K_RIGHT, K_d):(1, 0)})
-all_levels = [f"l{i}" for i in range(1, 7)] # all levels, in order
 
 # transforms a vector to position on a surface
 to_surface_pos = lambda pos, size: int_vector(surface_scale.elementwise()*vector(*pos)+surface_padding/2)
-
-
-# singleton, esentally a dict w/ some dynamic values
-class level_names_class:
-  # keys are level codes (used internally) and values are displayed names
-  def __init__(self, start=0):
-    self.data = {}
-    self.current_level = start
-  
-  def __getitem__(self, key):
-    try:
-      return self.data[key]
-    except KeyError:
-      pass
-    if key[0] == "l":
-      self.data[key] = "Level "+key[1:]
-    return self.data[key]
-  
-  def __next__(self):
-    self.current_level += 1
-    return all_levels[self.current_level]
-  
-  @property
-  def current(self):
-    return self[all_levels[self.current_level]]
 
 
 # basically a mix of javasctipt objects and default dicts, just for convience
